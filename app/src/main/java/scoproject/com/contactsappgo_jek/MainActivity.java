cc4c180f8fb38839574a7a51e5213ed0839dbc62
Ibnu Muzzakkir <personalict@gmail.com>
2017-05-18 07:05:51 +0700

    Starting Project

---
+++ app/src/main/java/scoproject/com/contactsappgo_jek/MainActivity.java
@@ -0,0 +1,13 @@
+package scoproject.com.contactsappgo_jek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
