bd2544c66873fa6fae2ab20eafca009a11163f4e
Ibnu Muzzakkir <personalict@gmail.com>
2017-05-18 13:27:56 +0700

    Add People Model using Realm Object

---
+++ app/src/main/java/scoproject/com/contactsappgojek/model/People.java
@@ -0,0 +1,21 @@
+package scoproject.com.contactsappgojek.model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by ibnumuzzakkir on 18/05/2017.
+ * Android Developer
+ * Garena Indonesia
+ */
+
+public class People extends RealmObject {
+    @PrimaryKey
+    public int id;
+    public String first_name;
+    public String last_name;
+    public String profile_pic;
+    public boolean favorite;
+    public String url;
+
+}
