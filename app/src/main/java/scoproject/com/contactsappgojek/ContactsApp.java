120054cf823249d6401f0ed39ed4976f9a69bbd5
Ibnu Muzzakkir <personalict@gmail.com>
2017-05-18 08:22:58 +0700

    Import MultiDex App library to gradle

---
+++ app/src/main/java/scoproject/com/contactsappgojek/ContactsApp.java
@@ -0,0 +1,10 @@
+package scoproject.com.contactsappgojek;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by ibnumuzzakkir on 5/18/17.
+ */
+
+public class ContactsApp extends MultiDexApplication{
+}
