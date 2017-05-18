5db0bba2ddfafc73272b8ffc526ed70a678ac5c6
Ibnu Muzzakkir <personalict@gmail.com>
2017-05-18 12:31:14 +0700

    Init app component and install multidex on Contacts Apps

--- app/src/main/java/scoproject/com/contactsappgojek/ContactsApp.java
+++ app/src/main/java/scoproject/com/contactsappgojek/ContactsApp.java
@@ -1,10 +1,29 @@
 package scoproject.com.contactsappgojek;
 
+import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
+
+import scoproject.com.contactsappgojek.di.component.AppComponent;
 
 /**
  * Created by ibnumuzzakkir on 5/18/17.
  */
 
 public class ContactsApp extends MultiDexApplication{
+    public static ContactsApp mInstance;
+
+    private AppComponent mAppComponent;
+
+    public static ContactsApp getApp() { return mInstance;}
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        MultiDex.install(this);
+        mInstance  = this;
+        mAppComponent = AppComponent.Initializer.init(this);
+    }
+
+    public AppComponent component(){ return mAppComponent;}
 }
